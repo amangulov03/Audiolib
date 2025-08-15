8ccd7305930ebebd880c88bbe627fc45dc382c63
Emir Amangulov <amangulov03@gmail.com>
2025-08-15 23:49:24 +0600

    for docs

--- config/urls.py
+++ config/urls.py
@@ -22,18 +22,22 @@ from django.conf.urls.static import static
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
+from rest_framework import permissions
+
 schema_view = get_schema_view(
     openapi.Info(
-        title="docs for front",
-        description="audiolibs_app",
+        title="Audiolib API Docs",
+        description="Документация API для фронтенда",
         default_version='v1'
-    )
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('docs/', schema_view.with_ui('swagger'))
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
