d2a7cf78be9f6eb8c19d4ac5453821af75ad601c
Emir Amangulov <amangulov03@gmail.com>
2025-08-24 23:52:22 +0600

    add filters and search

--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from .models import Genre, Book
 from .serializers import GenreSerializer, BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
@@ -9,3 +12,6 @@ class GenreViewSet(viewsets.ModelViewSet):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset_fields = ('genres',)
+    search_fields = ('title', )
